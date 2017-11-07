82d2daafc18300acef61383909584d9a58ca519c
PinkPonyKnight <32565920+PinkPonyKnight@users.noreply.github.com>
2017-11-07 19:39:41 +0100

    Tag Nacht Wechsel

---
+++ Zwergen-Clash-Royale/Assets/game/Scripts/Managers/DayNightManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightManager : MonoBehaviour {
+    public bool night = false;
+    public GameObject lightObject;
+    private Light light;
+    private float elapsed;
+    private float timeForEffect = 300.0f;
+
+    // Use this for initialization
+    void Start () {
+        elapsed = 0.0f;
+        light = lightObject.GetComponent<Light>();
+        light.intensity = 1.2f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(timeForEffect > elapsed)
+        {
+            if (night)
+            {
+                light.intensity = Mathf.Lerp(0.1f, 1.2f, elapsed / timeForEffect);
+            }
+            else
+            {
+                light.intensity = Mathf.Lerp(1.2f, 0.1f, elapsed / timeForEffect);
+            }
+
+            elapsed += Time.deltaTime;
+        }
+        else
+        {
+            timeForEffect = 300.0f;
+            if (night)
+            {
+                night = false;
+            }
+            else
+            {
+                night = true;
+            }
+        }
+	}
+
+}
