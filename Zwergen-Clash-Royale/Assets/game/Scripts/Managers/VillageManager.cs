dac3c54c014d756b1517861e988b0b280654f64a
Thorthespy <30324630+Thorthespy@users.noreply.github.com>
2017-11-19 19:36:06 +0100

    Preparing Village Manager

---
+++ Zwergen-Clash-Royale/Assets/game/Scripts/Managers/VillageManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillageManager : MonoBehaviour {
+
+    public Transform[] spawnPoints;
+    public float spawntime = 30f;
+    public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
