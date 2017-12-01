5608422bb9ac7327f2456c53cb1b8727ae345c31
PinkPonyKnight <32565920+PinkPonyKnight@users.noreply.github.com>
2017-12-01 13:01:31 +0100

    Revert "Merge branch 'UI' into Build2"
    This reverts commit e08aecd6435d1a44b02d58335be382fb23826cc8, reversing
    changes made to 77a17ec7114aa25861d33877df85cbc157faa448.

--- Zwergen-Clash-Royale/Assets/game/Scripts/Managers/DayNightManager.cs
+++ Zwergen-Clash-Royale/Assets/game/Scripts/Managers/DayNightManager.cs
@@ -8,7 +8,7 @@ public class DayNightManager : MonoBehaviour
     public GameObject lightObject;
     public GameObject parentObject;
 
-    private Light light;
+    Light light;
     private float elapsed;
 
     private float time;
@@ -65,11 +65,7 @@ public class DayNightManager : MonoBehaviour
         }
 
 
-<<<<<<< HEAD
         time = (float)light.intensity;
-=======
-       time = (float) light.intensity;
->>>>>>> UI
 
     }
 
