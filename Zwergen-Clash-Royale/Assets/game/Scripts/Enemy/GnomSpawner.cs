c558749da4b1e0ced8a6ab004d8b876048772646
Thorthespy <30324630+Thorthespy@users.noreply.github.com>
2017-12-06 21:15:27 +0100

    Gnom Spawner
    Added Spawner
    Village Manager has a List of Spawners
    Removed old Spawner

---
+++ Zwergen-Clash-Royale/Assets/game/Scripts/Enemy/GnomSpawner.cs
@@ -0,0 +1,82 @@
+﻿using CompleteProject;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GnomSpawner : MonoBehaviour {
+
+    public float spawntime = 30f;
+    public GameObject enemy;
+    public bool isInRange = false;
+    public bool isDead = false;
+    public float spawnTimer = 30f;
+
+    public GameObject gnom;
+    float timer = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        gnom = Spawn();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(!gnom && gnom.GetComponent<EnemyHealth>().isDead)
+        {
+            isDead = true;
+
+        }
+
+        if(isDead)
+        {
+            timer += Time.deltaTime;
+        }
+        
+        if(timer >= spawnTimer)
+        {
+            Spawn();
+            timer = 0f;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = true;
+            if (!gnom && !gnom.GetComponent<EnemyHealth>().isDead)
+                gnom.GetComponent<EnemyMovement>().RangeTrigger();
+                
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = true;
+            if (!gnom && !gnom.GetComponent<EnemyHealth>().isDead)
+                gnom.GetComponent<EnemyMovement>().RangeTrigger();
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = false;
+            if (!gnom && !gnom.GetComponent<EnemyHealth>().isDead)
+                gnom.GetComponent<EnemyMovement>().NotRangeTrigger();
+
+        }
+    }
+
+    GameObject Spawn() {
+        gnom = Instantiate(enemy, transform.position, transform.rotation);
+        return gnom;
+    }
+}
--- Zwergen-Clash-Royale/Assets/game/Scripts/Managers/VillageManager.cs
+++ Zwergen-Clash-Royale/Assets/game/Scripts/Managers/VillageManager.cs
@@ -5,89 +5,26 @@ using UnityEngine;
 
 public class VillageManager : MonoBehaviour {
 
-    public Transform spawnPoint;
-    public float spawntime = 30f;
-    public GameObject enemy;
-    public bool isInRange = false;
+    public Transform[] spawnPoints;
 
     List<GameObject> enemys = new List<GameObject> { };
-    int count = 0;
-
-    void Start () {
-        InvokeRepeating("Spawn", 0f, 2f);
-	}
-	
 
 	void FixedUpdate () {
-		for(int i=0; i< enemys.Count; i++)
+        
+		for(int i=0; i< spawnPoints.Length; i++)
         {
-            if (enemys[i].GetComponent<EnemyHealth>().isDead)
+            GameObject gnom = spawnPoints[i].GetComponent<GnomSpawner>().gnom;
+            if (!gnom && !gnom.GetComponent<EnemyHealth>().isDead)
             {
-                enemys.RemoveAt(i);
-            }
-        }
-	}
+                gnom.gameObject.transform.parent = gameObject.transform;
+                enemys.Add(gnom);
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isInRange = true;
-            if (enemys.Count > 0)
+            } else if(gnom.GetComponent<EnemyHealth>().isDead)
             {
-                for (int i = 0; i < enemys.Count; i++)
-                {
-                    if (!enemys[i].GetComponent<EnemyHealth>().isDead)
-                        enemys[i].GetComponent<EnemyMovement>().RangeTrigger();
-                }
+                enemys.Remove(gnom);
             }
-        }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isInRange = true;
-            if (enemys.Count > 0)
-            {
-                for (int i = 0; i < enemys.Count; i++)
-                {
-                    if (!enemys[i].GetComponent<EnemyHealth>().isDead)
-                        enemys[i].GetComponent<EnemyMovement>().RangeTrigger();
-                }
-            }
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            if (enemys.Count > 0)
-            {
-                for (int i = 0; i < enemys.Count; i++)
-                {
-                    if (!enemys[i].GetComponent<EnemyHealth>().isDead)
-                        enemys[i].GetComponent<EnemyMovement>().NotRangeTrigger();
-                }
-            }
-            
-            isInRange = false;
-        }
-    }
-
-    
-
-
-    void Spawn()
-    {
-        if(enemys.Count<3)
-        {
-            enemys.Add(Instantiate(enemy, spawnPoint.position, spawnPoint.rotation));
-            
-        }
+        } 
 
     }
-    
+ 
 }
