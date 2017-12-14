62ef34b1c37c87985916f36f10c4757813452e86
PinkPonyKnight <32565920+PinkPonyKnight@users.noreply.github.com>
2017-12-14 14:06:09 +0100

    Skeleton is now a NightEnemy

---
+++ Zwergen-Clash-Royale/Assets/game/Scripts/Enemy/NightEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CompleteProject;
+
+public class NightEnemy : MonoBehaviour {
+    private GameObject enemyHealth;
+	// Use this for initialization
+	void Start () {
+        enemyHealth = FindObjectOfType<EnemyHealth>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!FindObjectOfType<DayNightManager>().night)
+        {
+            enemyHealth.Damage(100);
+        }
+	}
+}
