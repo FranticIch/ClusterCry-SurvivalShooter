39b5fb6aa0e07a6f15fce44e6e324a15032bd5ce
PinkPonyKnight <32565920+PinkPonyKnight@users.noreply.github.com>
2017-12-18 10:59:48 +0100

    Added a Panel for a Introduction to the FoodBranch

---
+++ Zwergen-Clash-Royale/Assets/game/Scripts/Managers/UIManagers/StartingDialog.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingDialog : MonoBehaviour {
+    public Canvas _startingMenuCanvas;
+    private void Start()
+    {
+    }
+        public void CloseStartingMenu()
+    {
+        _startingMenuCanvas.enabled = false;
+        
+    }
+}
